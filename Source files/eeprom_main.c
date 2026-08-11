4514c5584ef9adcef07e02401533fc775d08bd33
manaswinimethari <manaswinimethari@gmail.com>
2026-08-11 12:17:18 -0400

    Update eeprom_main.c

--- Source files/eeprom_main.c
+++ Source files/eeprom_main.c
@@ -1,44 +1,141 @@
-#include<lpc21xx.h>
-#include"i2c.h"
-#include"i2c_eeprom.h"
-#include"lcd.h"
-#include"delay.h"
-
-char usr_id[][5]={"1111","2222","3333","4444"};
-char usr_pass[][5]={"5555","6666","7777","8888"};
+#include <lpc21xx.h>       // LPC21xx microcontroller header
+#include "i2c.h"           // I2C driver functions
+#include "i2c_eeprom.h"    // I2C EEPROM functions
+#include "lcd.h"           // LCD driver functions
+#include "delay.h"         // Delay functions
+/* 
+ * Array containing 4 user IDs.
+ * Each ID contains 4 characters + 1 NULL character.
+ */
+char usr_id[][5] = {"1111","2222","3333","4444"};
+/*
+ * Array containing 4 user passwords.
+ * Each password contains 4 characters + 1 NULL character.
+ */
+char usr_pass[][5] = {"5555","6666","7777","8888"};
+/*
+ * Buffer used to store the user ID/password
+ * read from the EEPROM.
+ */
 char rd_usr_id[5];
-
 int main()
 {
-	int i;
-	init_lcd();
-	init_i2c();
-	str_lcd("USER ID'S:");
-	for(i=0;i<4;i++)
-	{
-		i2c_eeprom_write_page(0x50,0x0000+(i*5),usr_id[i],4);
-		delay_ms(100);
-	}
-	for(i=0;i<4;i++)
-	{
-		i2c_eeprom_seq_read(0x50,0x0000+(i*5),rd_usr_id,4);
-		cmd_lcd(0xC0);
-		str_lcd((u8 *)rd_usr_id);
-		delay_ms(1000);
-	}
-	cmd_lcd(0x01);
-	str_lcd("USER PASS:");
-	for(i=0;i<4;i++)
-	{
-		i2c_eeprom_write_page(0x50,0x0020+(i*5),usr_pass[i],4);
-		delay_ms(100);
-	}
-	for(i=0;i<4;i++)
-	{
-		i2c_eeprom_seq_read(0x50,0x0020+(i*5),rd_usr_id,4);
-		cmd_lcd(0xC0);
-		str_lcd((u8 *)rd_usr_id);
-		delay_ms(1000);
-	}
-	while(1);
+    int i;
+    /* Initialize the LCD */
+    init_lcd();
+    /* Initialize the I2C peripheral */
+    init_i2c();
+    /* Display heading on LCD */
+    str_lcd("USER ID'S:");
+    /*
+     * Write all 4 user IDs into EEPROM.
+     *
+     * EEPROM device address = 0x50
+     *
+     * Starting EEPROM address:
+     * User 1 -> 0x0000
+     * User 2 -> 0x0005
+     * User 3 -> 0x000A
+     * User 4 -> 0x000F
+     *
+     * 5 bytes are reserved for each ID,
+     * although only 4 characters are written.
+     */
+    for(i = 0; i < 4; i++)
+    {
+        i2c_eeprom_write_page(
+            0x50,               // EEPROM device address
+            0x0000 + (i * 5),   // EEPROM memory address
+            usr_id[i],          // ID to be written
+            4                   // Number of bytes to write
+        );
+
+        /* Wait for EEPROM write operation to complete */
+        delay_ms(100);
+    }
+    /*
+     * Read all 4 user IDs from EEPROM
+     * and display them on the LCD.
+     */
+    for(i = 0; i < 4; i++)
+    {
+        /* Read 4 bytes from EEPROM */
+        i2c_eeprom_seq_read(
+            0x50,               // EEPROM device address
+            0x0000 + (i * 5),   // EEPROM memory address
+            rd_usr_id,          // Buffer to store received data
+            4                   // Number of bytes to read
+        );
+        /*
+         * Add NULL character at the end.
+         * This is required because str_lcd() expects
+         * a NULL-terminated string.
+         */
+        rd_usr_id[4] = '\0';
+
+        /* Move LCD cursor to second line */
+        cmd_lcd(0xC0);
+
+        /* Display the ID read from EEPROM */
+        str_lcd((u8 *)rd_usr_id);
+
+        /* Keep the ID on the LCD for 1 second */
+        delay_ms(1000);
+    }
+    /* Clear the LCD */
+    cmd_lcd(0x01);
+    /* Display password heading */
+    str_lcd("USER PASS:");
+	/*
+     * Write all 4 user passwords into EEPROM.
+     *
+     * Starting EEPROM address:
+     * Password 1 -> 0x0020
+     * Password 2 -> 0x0025
+     * Password 3 -> 0x002A
+     * Password 4 -> 0x002F
+     */
+    for(i = 0; i < 4; i++)
+    {
+        i2c_eeprom_write_page(
+            0x50,               // EEPROM device address
+            0x0020 + (i * 5),  // EEPROM memory address
+            usr_pass[i],       // Password to be written
+            4                   // Number of bytes to write
+        );
+
+        /* Wait for EEPROM write operation */
+        delay_ms(100);
+    }
+    /*
+     * Read all 4 passwords from EEPROM
+     * and display them on the LCD.
+     */
+    for(i = 0; i < 4; i++)
+    {
+        /* Read 4 bytes from EEPROM */
+        i2c_eeprom_seq_read(
+            0x50,               // EEPROM device address
+            0x0020 + (i * 5),  // EEPROM memory address
+            rd_usr_id,          // Reuse buffer for password
+            4                   // Number of bytes to read
+        );
+        /*
+         * Add NULL character so that the data
+         * can be displayed as a string.
+         */
+        rd_usr_id[4] = '\0';
+        /* Move LCD cursor to second line */
+        cmd_lcd(0xC0);
+        /* Display the password read from EEPROM */
+        str_lcd((u8 *)rd_usr_id);
+
+        /* Keep password on LCD for 1 second */
+        delay_ms(1000);
+    }
+    /*
+     * Infinite loop.
+     * Program remains here after all operations are complete.
+     */
+    while(1);
 }
